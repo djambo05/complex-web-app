da5dca0e4b4101b96ff3f5dd3bc53d2223768fd1
Djamalutin <fisher.gerryy@mail.ru>
2023-05-29 20:48:39 +0300

    feat: DataContext Provider

--- src/DataContext.js
+++ src/DataContext.js
@@ -1,1 +1,18 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
+
+const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState({});
+
+  const setValues = (values) => {
+    setData((prevData) => ({ ...prevData, ...values }));
+  };
+  return (
+    <DataContext.Provider value={(data, setValues)}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useData = () => useContext(DataContext);
